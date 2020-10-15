2760a0fc09d4d42c218ee44ec0b01135e451296a
karollyne <weelly2704@gmail.com>
2020-10-15 12:09:06 -0300

    testanto metodos no combo box

--- src/view/Tela.java
+++ src/view/Tela.java
@@ -5,13 +5,15 @@
  */
 package view;
 
+import javax.swing.ImageIcon;
+
 /**
  *
  * @author karol
  */
 public class Tela extends javax.swing.JFrame {
 
-    String a[] = new String[]{"Lua", "Júpiter ", "Marte ", "Mercúrio", "Netuno", "Plutão",
+    String a[] = new String[]{"","Lua", "Júpiter ", "Marte ", "Mercúrio", "Netuno", "Plutão",
         "Saturno", "Urano", "Vênus"};
 
     public Tela() {
@@ -22,11 +24,12 @@ public class Tela extends javax.swing.JFrame {
         iniciaLista();
     }
 
-    private void iniciaLista(){
-        for (String a : this.a){
+    private void iniciaLista() {
+        for (String a : this.a) {
             jcbPlanetas.addItem(a);
         }
     }
+
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
@@ -41,7 +44,7 @@ public class Tela extends javax.swing.JFrame {
         jlVariante = new javax.swing.JLabel();
         jlPeso = new javax.swing.JLabel();
         jtfPeso = new javax.swing.JTextField();
-        jButton1 = new javax.swing.JButton();
+        jbCalcular = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setPreferredSize(new java.awt.Dimension(600, 500));
@@ -55,10 +58,18 @@ public class Tela extends javax.swing.JFrame {
 
         jcbPlanetas.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
         jcbPlanetas.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        jcbPlanetas.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jcbPlanetasMouseClicked(evt);
+            }
+        });
 
         jLabel4.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
         jLabel4.setForeground(new java.awt.Color(255, 255, 255));
         jLabel4.setText("Escolha um:");
+
+        jlPlanetaVariante.setIcon(new javax.swing.ImageIcon(getClass().getResource("/pictures/preto.png"))); // NOI18N
+        jlPlanetaVariante.setPreferredSize(new java.awt.Dimension(200, 120));
 
         jlTerra.setForeground(new java.awt.Color(255, 255, 255));
         jlTerra.setIcon(new javax.swing.ImageIcon(getClass().getResource("/pictures/terra.png"))); // NOI18N
@@ -88,17 +99,17 @@ public class Tela extends javax.swing.JFrame {
             }
         });
 
-        jButton1.setBackground(new java.awt.Color(0, 0, 0));
-        jButton1.setFont(new java.awt.Font("Tahoma", 0, 16)); // NOI18N
-        jButton1.setForeground(new java.awt.Color(255, 255, 255));
-        jButton1.setText("Calcular");
-        jButton1.setBorderPainted(false);
-        jButton1.setContentAreaFilled(false);
-        jButton1.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
-        jButton1.setFocusPainted(false);
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        jbCalcular.setBackground(new java.awt.Color(0, 0, 0));
+        jbCalcular.setFont(new java.awt.Font("Tahoma", 0, 16)); // NOI18N
+        jbCalcular.setForeground(new java.awt.Color(255, 255, 255));
+        jbCalcular.setText("Calcular");
+        jbCalcular.setBorderPainted(false);
+        jbCalcular.setContentAreaFilled(false);
+        jbCalcular.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        jbCalcular.setFocusPainted(false);
+        jbCalcular.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                jbCalcularActionPerformed(evt);
             }
         });
 
@@ -125,10 +136,10 @@ public class Tela extends javax.swing.JFrame {
                             .addGroup(jPanel1Layout.createSequentialGroup()
                                 .addGap(20, 20, 20)
                                 .addComponent(jlTerra, javax.swing.GroupLayout.PREFERRED_SIZE, 132, javax.swing.GroupLayout.PREFERRED_SIZE))
-                            .addComponent(jButton1))
+                            .addComponent(jbCalcular))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 156, Short.MAX_VALUE)
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jlPlanetaVariante)
+                            .addComponent(jlPlanetaVariante, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                                 .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                                     .addComponent(jlPadrao)
@@ -153,17 +164,17 @@ public class Tela extends javax.swing.JFrame {
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(jButton1)
+                        .addComponent(jbCalcular)
                         .addGap(45, 45, 45)
                         .addComponent(jlTerra, javax.swing.GroupLayout.PREFERRED_SIZE, 120, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGap(20, 20, 20)
-                        .addComponent(jlPlanetaVariante)
+                        .addComponent(jlPlanetaVariante, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(54, 54, 54)
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                             .addComponent(jlVariante)
                             .addComponent(jlPadrao))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 133, Short.MAX_VALUE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 13, Short.MAX_VALUE)
                         .addComponent(jlPeso)))
                 .addGap(120, 120, 120))
         );
@@ -183,10 +194,10 @@ public class Tela extends javax.swing.JFrame {
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+    private void jbCalcularActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbCalcularActionPerformed
 
-        
-    }//GEN-LAST:event_jButton1ActionPerformed
+
+    }//GEN-LAST:event_jbCalcularActionPerformed
 
     private void jtfPesoKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_jtfPesoKeyTyped
         if (!(evt.getKeyChar() + "").matches("[0-9-, ]")) {
@@ -194,6 +205,12 @@ public class Tela extends javax.swing.JFrame {
         }
 
     }//GEN-LAST:event_jtfPesoKeyTyped
+
+    private void jcbPlanetasMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jcbPlanetasMouseClicked
+        
+
+
+    }//GEN-LAST:event_jcbPlanetasMouseClicked
 
     /**
      * @param args the command line arguments
@@ -231,10 +248,10 @@ public class Tela extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JButton jbCalcular;
     private javax.swing.JComboBox<String> jcbPlanetas;
     private javax.swing.JLabel jlPadrao;
     private javax.swing.JLabel jlPeso;
